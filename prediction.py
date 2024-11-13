07d35b0b27846a611af3d7bde3cdcaee30cd41ed
Sahith-Rao <sahithraolingampally@gmail.com>
2024-11-13 16:53:51 +0530

    model loaded to prediction file

---
+++ prediction.py
@@ -0,0 +1,18 @@
+import cv2
+from keras.models import model_from_json
+import numpy as np
+
+json_file = open("emotiondetector.json", "r")
+model_json = json_file.read()
+json_file.close()
+model = model_from_json(model_json)
+
+model.load_weights("emotiondetector.h5")
+haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade=cv2.CascadeClassifier(haar_file)
+
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1,48,48,1)
+    return feature/255.0
+
