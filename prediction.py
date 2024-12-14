8e7938c6985fbc7b196bee487dcee2282cbe458e
Sahith-Rao <sahithraolingampally@gmail.com>
2024-12-14 09:19:10 +0530

    server for api is initialized

--- prediction.py
+++ prediction.py
@@ -1,16 +1,13 @@
 import cv2
 from keras.models import model_from_json
 import numpy as np
-
 json_file = open("emotiondetector.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
-
 model.load_weights("emotiondetector.h5")
 haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade=cv2.CascadeClassifier(haar_file)
-
 def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1,48,48,1)
@@ -29,8 +26,6 @@ while True:
             img = extract_features(image)
             pred = model.predict(img)
             prediction_label = labels[pred.argmax()]
-            # print("Predicted Output:", prediction_label)
-            # cv2.putText(im,prediction_label)
             cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
         cv2.imshow("Output",im)
         cv2.waitKey(27)
