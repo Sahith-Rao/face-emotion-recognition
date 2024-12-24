4dc94f4e0282f71f2b0faf0dc75bdef9f701aae5
Sahith-Rao <sahithraolingampally@gmail.com>
2024-12-24 09:14:30 +0530

    UI updated

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ def home():
 
 labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 
+@app.route('/detection')
+def detection():
+    return render_template('detection.html')
+
+    
+
 @app.route('/predictemotion', methods=['POST'])
 def predictemotion():
     data = request.get_json()
